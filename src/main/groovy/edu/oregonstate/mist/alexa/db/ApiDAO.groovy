2c86ea77d97d1851cbbb14fab597ba733e8aba25
floppyears <cedenoj@onid.oregonstate.edu>
2017-05-12 13:36:27 -0700

    Add open restaurants to locations dao

--- src/main/groovy/edu/oregonstate/mist/alexa/db/ApiDAO.groovy
+++ src/main/groovy/edu/oregonstate/mist/alexa/db/ApiDAO.groovy
@@ -13,4 +13,30 @@ class ApiDAO {
         requests = new Requests(httpClient, alexaConfiguration['oauth2Url'].toString(),
                 alexaConfiguration['oauth2Params'].toString())
     }
+
+    /**
+     * Takes a list and joins the elements using commas for all the elements, but uses
+     * "and" for the last element.
+     *
+     * @param list
+     * @return
+     */
+    static String joinList(def list) {
+        if (!list) {
+             return "Sorry, the list is empty"
+        }
+
+        def listText = ""
+        list.each {
+            if (it == list.last()) {
+                listText += " and "
+            } else if (it != list.first()) {
+                listText += ", "
+            }
+
+            listText += it
+        }
+
+        listText
+    }
 }
--- src/main/groovy/edu/oregonstate/mist/alexa/db/LocationsDAO.groovy
+++ src/main/groovy/edu/oregonstate/mist/alexa/db/LocationsDAO.groovy
@@ -4,8 +4,30 @@ import groovy.transform.InheritConstructors
 
 @InheritConstructors
 class LocationsDAO extends ApiDAO {
+
+    public static final NO_OPEN_RESTAURANTS = "Sorry. There are no open restaurants."
+
     String getOpenRestaurants() {
-        "hackathon 2017"
+        def urlPrefix = alexaConfiguration['locationsUrl'].toString()
+        String url = urlPrefix + "?type=dining&isOpen=true"
+        def jsonApiObject = requests.get(url)
+
+        // check if it's NO_RESULTS a 404 or empty body
+        if (!jsonApiObject) {
+            return NO_OPEN_RESTAURANTS
+        }
+
+        if (!jsonApiObject['data']) {
+            return NO_OPEN_RESTAURANTS
+        }
+
+        //@todo: handle multiple results
+        def restaurantList = []
+        jsonApiObject['data'].each {
+            restaurantList += it['attributes']['name']
+        }
+
+        "The following restaurants are open: " + joinList(restaurantList)
     }
 
     String getNearbyRestaurants() {
