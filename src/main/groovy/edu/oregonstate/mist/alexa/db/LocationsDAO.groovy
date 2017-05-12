85938fc54a2a1722588d14607f2600e80b0cc1eb
floppyears <cedenoj@onid.oregonstate.edu>
2017-05-12 14:22:50 -0700

    OpenHours for location

--- src/main/groovy/edu/oregonstate/mist/alexa/db/LocationsDAO.groovy
+++ src/main/groovy/edu/oregonstate/mist/alexa/db/LocationsDAO.groovy
@@ -2,9 +2,10 @@ package edu.oregonstate.mist.alexa.db
 
 import groovy.transform.InheritConstructors
 
+import java.time.LocalDate
+
 @InheritConstructors
 class LocationsDAO extends ApiDAO {
-
     public static final NO_OPEN_RESTAURANTS = "Sorry. There are no open restaurants."
 
     String getOpenRestaurants() {
@@ -13,15 +14,10 @@ class LocationsDAO extends ApiDAO {
         def jsonApiObject = requests.get(url)
 
         // check if it's NO_RESULTS a 404 or empty body
-        if (!jsonApiObject) {
+        if (!jsonApiObject || !jsonApiObject['data']) {
             return NO_OPEN_RESTAURANTS
         }
 
-        if (!jsonApiObject['data']) {
-            return NO_OPEN_RESTAURANTS
-        }
-
-        //@todo: handle multiple results
         def restaurantList = []
         jsonApiObject['data'].each {
             restaurantList += it['attributes']['name']
@@ -32,5 +28,47 @@ class LocationsDAO extends ApiDAO {
 
     String getNearbyRestaurants() {
         "hackathon 2017"
+    }
+
+    /**
+     * Returns a string in the form of "The locationQuery is open today from x to y"
+     *
+     * @param locationQuery
+     * @return
+     */
+    String openHoursForLocation(String locationQuery) {
+        def notOpenToday = "Sorry, ${locationQuery} is not open today."
+        def couldntFindIt = "Sorry, I couldn't find: ${locationQuery}"
+
+        def urlPrefix = alexaConfiguration['locationsUrl'].toString()
+        String url = urlPrefix + "?q=" + locationQuery
+        def jsonApiObject = requests.get(url)
+
+        // check if it's NO_RESULTS a 404 or empty body
+        if (!jsonApiObject || !jsonApiObject['data']) {
+            println "DEBUG: Couldn't find the result at all"
+            return couldntFindIt
+        }
+
+        // @todo: handle multiple results
+
+        // Check if we have open hours at all
+        def attributes = jsonApiObject['data'][0]['attributes']
+        def doesntHaveHours = !attributes['openHours']
+        if (doesntHaveHours) {
+            println "DEBUG: The location doesn't have any hours"
+            return notOpenToday
+        }
+
+        // Check if we have open hours for today
+        LocalDate now = LocalDate.now()
+        String dayOfWeek = now.getDayOfWeek().getValue() - 1
+        def todayHours = attributes['openHours'][dayOfWeek]
+        if (!todayHours) {
+            return notOpenToday
+        }
+
+        //@todo: convert hours to human values!
+        "${locationQuery} is open today from ${todayHours[0]['start']} to ${todayHours[0]['end']}."
     }
 }
